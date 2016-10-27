9f73df0997cc450c4e2da0ce1e50c207b4b964c5
Sergey Bekrin <sergey@bekrin.me>
2016-10-27 12:47:43 +0300

    Add last update date and source code link to footer

--- app/components/Footer/Footer.js
+++ app/components/Footer/Footer.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import Link from 'components/Link';
 import './Footer.styl';
 
 export const Footer = () => (
-    <footer className="Footer">SB {new Date().getFullYear()}</footer>
+    <footer className="Footer">
+        Last update {__LAST_COMMIT_DATE__}<span> &bull; </span>
+        <Link
+            to="//github.com/sergeybekrin/bekrin.me"
+            eventLabel="github-source"
+        >View source code</Link>
+    </footer>
 );
 
 export default Footer;
--- webpack/shared.js
+++ webpack/shared.js
@@ -1,5 +1,10 @@
+import { execSync } from 'child_process';
 import path from 'path';
+import moment from 'moment';
 import webpack from 'webpack';
+
+const lastCommitDate = execSync('git log -1 --format=%ai').toString();
+const formattedLastCommitDate = moment(lastCommitDate).fromNow();
 
 export default {
     output: {
@@ -18,7 +23,8 @@ export default {
         new webpack.DefinePlugin({
             'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
             __DEV__: process.env.NODE_ENV === 'development',
-            __PROD__: process.env.NODE_ENV === 'production'
+            __PROD__: process.env.NODE_ENV === 'production',
+            __LAST_COMMIT_DATE__: JSON.stringify(formattedLastCommitDate)
         })
     ]
 };
